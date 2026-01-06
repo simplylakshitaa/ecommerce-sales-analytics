d1d6b0dc9c6a52f165737f064d4588a4da18d995
simplylakshitaa <lakshitaaparashar@gmail.com>
2026-01-06 17:25:09 +0530

    orders data cleaning with datetime conversion

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -13,22 +13,38 @@ def clean_customers():
 
     df = pd.read_csv(os.path.join(DATA_RAW, "olist_customers_dataset.csv"))
     print("Raw shape:", df.shape)
-
-  
     df = df.drop_duplicates(subset="customer_id")
-
-    
     df["customer_city"] = df["customer_city"].str.lower().str.strip()
     df["customer_state"] = df["customer_state"].str.lower().str.strip()
-
-
     df.to_csv(os.path.join(DATA_CLEANED, "customers_cleaned.csv"), index=False)
+    print("Cleaned shape:", df.shape)
+
+def clean_orders():
+    print("\n--- Cleaning Orders ---")
+    df = pd.read_csv(os.path.join(DATA_RAW, "olist_orders_dataset.csv"))
+    print("Raw shape:", df.shape)
+    df = df.dropna(subset=["order_id"])
+    date_cols = [
+        "order_purchase_timestamp",
+        "order_approved_at",
+        "order_delivered_carrier_date",
+        "order_delivered_customer_date",
+        "order_estimated_delivery_date"
+    ]
+
+    for col in date_cols:
+        df[col] = pd.to_datetime(df[col], errors="coerce")
+
+    df["order_status"] = df["order_status"].str.lower().str.strip()
+
+    df.to_csv(os.path.join(DATA_CLEANED, "orders_cleaned.csv"), index=False)
     print("Cleaned shape:", df.shape)
 
 
 def main():
     print("Starting data cleaning pipeline...")
     clean_customers()
+    clean_orders()
     print("Data cleaning completed successfully.")
 
 
